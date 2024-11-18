15553ed57aef0c49fc6b2a10de43c0d9a33ecf6d
Benjamin BENOIT <benjaminbenoit@MacBook-Air-de-Benjamin.local>
2024-11-18 01:43:08 -0500

    products mieux affichés?

--- next.config.js
+++ next.config.js
@@ -4,12 +4,16 @@ const nextConfig = {
   swcMinify: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'css'],
   images: {
-    domains: ['images.unsplash.com'],
+    domains: [
+      'images.unsplash.com',
+      'matinducoin-backend-b2f47bd8118b.herokuapp.com'  // Ajoutez cette ligne
+    ],
+    unoptimized: true  // Ajoutez cette option pour Vercel
   },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.node$/,
-      use: 'ignore-loader', // Ignore les fichiers .node
+      use: 'ignore-loader',
     });
 
     return config;
